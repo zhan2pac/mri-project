21c37626e19b76b1ebec3a7a109cb7268af01f6a
zhan2pac <zhumatauev@gmail.com>
2024-10-31 02:26:22 +0000

    add transforms, subject dataset

--- datasets/iseg_dataset.py
+++ datasets/iseg_dataset.py
@@ -1,17 +1,19 @@
 from collections import defaultdict
 from pathlib import Path
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 import torch
 import torchio as tio
 from utils import ROOT_PATH
 import SimpleITK as sitk
 import torch.nn.functional as F
 
+from .transforms import transforms
+
 sitk.ProcessObject_SetGlobalWarningDisplay(False)
 
 
 class iSegDataset(Dataset):
-    def __init__(self, config, mode="train"):
+    def __init__(self, config, transforms, mode="train"):
         self.mode = mode
         self.config = config
 
@@ -21,15 +23,13 @@ class iSegDataset(Dataset):
         elif mode == "test":
             self.data_path = self.data_path / "iSeg-2019-Validation"
 
-        self.transforms = None
-        self.resize = tio.transforms.Resize((192, 192, 256))
-
-        self.images = []
-        self.labels = []
+        self.transforms = transforms
+        self.subjects = []
         self.load_data()
-
-    def normalize(self, image):
-        return (image - image.min()) / (image.max() - image.min())
+        if mode == "train":
+            self.subjects = self.subjects[:6]
+        elif mode == "val":
+            self.subjects = self.subjects[6:]
 
     def load_data(self):
         data = defaultdict(dict)
@@ -37,69 +37,73 @@ class iSegDataset(Dataset):
         for subject_path in self.data_path.iterdir():
             if subject_path.suffix == ".img":
                 _, idx, type = subject_path.stem.split("-")
-                data[int(idx)][type] = tio.ScalarImage(subject_path)
+                data[int(idx)][type] = subject_path
 
-        for idx, subject in data.items():
-            subject = data[idx]
-            t1_image = subject["T1"].tensor
-            t2_image = subject["T2"].tensor
-
-            image = torch.cat([t1_image, t2_image], dim=0)
-            image = self.resize(image)
-            image = self.normalize(image.unsqueeze(0))  # [1, channels=2, depth, height, width]
+        for idx, subject_dict in data.items():
+            t1_image = tio.ScalarImage(subject_dict["T1"])
+            t2_image = tio.ScalarImage(subject_dict["T2"])
+            joined_image = torch.cat([t1_image.tensor, t2_image.tensor], dim=0)
 
             if self.mode == "test":
-                self.images.append(image)
-                continue
+                subject = tio.Subject(
+                    image=tio.ScalarImage(tensor=joined_image),  # [channels=2, depth, height, width]
+                )
+            else:
+                subject = tio.Subject(
+                    image=tio.ScalarImage(tensor=joined_image),  # [channels=2, depth, height, width]
+                    label=tio.ScalarImage(subject_dict["label"]),  # [1, depth, height, width]
+                )
 
-            label_image = subject["label"].tensor
-            label_image = self.resize(label_image).long()  # [1, depth, height, width]
-
-            if self.mode == "train" and (idx <= 6):
-                self.images.append(image)
-                self.labels.append(label_image)
-
-            elif self.mode == "val" and (idx > 6):
-                self.images.append(image)
-                self.labels.append(label_image)
+            self.subjects.append(subject)
 
     def __len__(self):
-        return len(self.images)
+        return len(self.subjects)
 
     def __getitem__(self, idx):
         """
         There are 2 channels: T1 and T2
-        Returns:
-            - image (Tensor): image of size [1, channels, depth, height, width]
-            - label (Tensor): labeled pixels of size [1, depth, height, width]
+        Returns dict of:
+            - image (Tensor): image of size [channels, depth, height, width]
+            - label (Tensor): labeled pixels of size [depth, height, width]
         """
-        image = self.images[idx]
+        subject = self.subjects[idx]
         if self.transforms is not None:
-            image = self.transforms(image)
+            subject = self.transforms(subject)
 
-        if self.mode == "test":
-            return image
+        image = subject.image.tensor
+        label = subject.label.tensor.squeeze(0) if self.mode != "test" else None
 
-        label = self.labels[idx]
-        return image, label
+        return {"image": image, "label": label}
 
 
-def collate_fn(batch: list[tuple]):
-    batched_images = []
-    batched_labels = []
+def collate_fn(batch: list[dict]):
+    """
+    Returns:
+        - images (Tensor): image of size [batch_size, channels, depth, height, width]
+        - labels (Tensor): labeled pixels of size [batch_size, depth, height, width]
+    """
+    images = []
+    labels = []
 
-    for image, label in batch:
-        batched_images.append(image)
-        batched_labels.append(label)
+    for item in batch:
+        images.append(item["image"])
+        if item["label"] is not None:
+            labels.append(item["label"])
 
-    batched_images = torch.cat(batched_images, dim=0)
-    batched_labels = torch.cat(batched_labels, dim=0)
-    return batched_images, batched_labels
+    images = torch.stack(images, dim=0)
+    labels = torch.stack(labels, dim=0) if len(labels) else None
+    return images, labels
 
 
 def get_dataloader(config, mode="train"):
-    dataset = iSegDataset(config, mode=mode)
-    dataloader = DataLoader(
+    dataset = iSegDataset(
+        config,
+        transforms=transforms(),
+        mode=mode,
+    )
+
+    print(f"Dataset partition {mode} of size: {len(dataset)}")
+    dataloader = tio.SubjectsLoader(
         dataset,
         shuffle=(mode == "train"),
         collate_fn=collate_fn,
--- datasets/transforms.py
+++ datasets/transforms.py
@@ -2,32 +2,37 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import cv2
 
+import torchio as tio
 
-def transforms(probs=0.25):
-    return A.Compose([
-        A.HorizontalFlip(p=0.5),
-        A.VerticalFlip(p=0.5),
-        A.RandomRotate90(),
-        A.OneOf([
-            A.OpticalDistortion(
-                distort_limit=0.3,
-                border_mode=cv2.BORDER_CONSTANT,
-                value=0
+
+def transforms():
+    return tio.transforms.Compose(
+        [
+            tio.transforms.RandomFlip(axes=(0, 1, 2), flip_probability=0.5),
+            tio.transforms.Resize((160, 192, 256)),  # original size was (144, 192, 256)
+            tio.transforms.RescaleIntensity(out_min_max=(0, 1), exclude=["label"]),
+        ]
+    )
+
+
+def transforms2d(probs=0.25):
+    return A.Compose(
+        [
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.RandomRotate90(),
+            A.OneOf(
+                [
+                    A.OpticalDistortion(distort_limit=0.3, border_mode=cv2.BORDER_CONSTANT, value=0),
+                    A.GridDistortion(num_steps=5, distort_limit=0.3, border_mode=cv2.BORDER_CONSTANT, value=0),
+                ],
+                p=0.1,
             ),
-            A.GridDistortion(
-                num_steps=5,
-                distort_limit=0.3,
-                border_mode=cv2.BORDER_CONSTANT,
-                value=0
-            ),
-        ], p=0.1),
-        A.ColorJitter(),
-        A.Blur(blur_limit=2),
-    ])
+            A.ColorJitter(),
+            A.Blur(blur_limit=2),
+        ]
+    )
 
 
-def preprocess():
-    return A.Compose([
-        A.Normalize(mean=[0.776, 0.631, 0.737], std=[0.131, 0.166, 0.127]),
-        ToTensorV2()
-    ])
+def preprocess2d():
+    return A.Compose([A.Normalize(mean=[0.776, 0.631, 0.737], std=[0.131, 0.166, 0.127]), ToTensorV2()])
